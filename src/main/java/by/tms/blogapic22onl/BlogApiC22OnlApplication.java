4b4b0e1d6e6960eb7d337b3d0b2c808fe1e5363e
Simon Pirko <s.pirko@spotis.app>
2023-12-07 20:59:25 +0300

    first commit

---
+++ src/main/java/by/tms/blogapic22onl/BlogApiC22OnlApplication.java
@@ -0,0 +1,13 @@
+package by.tms.blogapic22onl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiC22OnlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiC22OnlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/tms/blogapic22onl/BlogApiC22OnlApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.blogapic22onl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiC22OnlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
